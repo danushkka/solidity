661f966eba4458e5571812577fa155345aa67e90
danushkka <daniko@Laptop-Dani.localdomain>
2025-10-06 17:02:14 +0300

    My first commit :)

---
+++ foundry-fund-me-f25/script/HelperConfig.s.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract addresses across different chains
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
